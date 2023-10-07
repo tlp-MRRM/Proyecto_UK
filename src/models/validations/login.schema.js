af02f5fa7fba61cb91550a84a09b3138e83ed951
Meyer <meyer-hector@hotmail.com>
2023-10-07 13:07:06 -0300

    feat: se agregaron mas validaciones al login.schema

--- src/models/validations/login.schema.js
+++ src/models/validations/login.schema.js
@@ -4,33 +4,16 @@ export const registerUserSchema = [
     body('email')
         .exists().withMessage('El email es requerido')
         .isEmpty().withMessage('El email no puede estar vacío')
-        .isEmail().withMessage('El email debe ser valido')
+        .isEmail().withMessage('El email debe ser valido'),
+    body('password')
+        .exists().withMessage('La contraseña es requerida')
+        .notEmpty().withMessage('La contraseña no puede estar vacía')
+        .isString().withMessage('La contraseña debe ser alfanumerica')
+        .isLength({ min: 8 }).withMessage('La contraseña debe tener minimo 8 caracteres')
+        .bail().matches(/[a-z]/).withMessage('La contraseña debe tener minimo 1 minuscula')
+        .bail().matches(/[A-Z]/).withMessage('La contraseña debe tener minimo 1 mayuscula')
+        .bail().matches(/[0-9]/).withMessage('La contraseña debe tener minimo 1 numero')
+        .bail().matches(/[^A-Za-z0-9]/).withMessage('La contraseña debe tener al menos un signo'),
 ]
 
 
-
-
-checkSchema({
-
-    email: {
-        notEmpty: {
-            errorMessage: "El email es obligatorio"
-        },
-        isEmail: {
-            errorMessage: "El email es inválido"
-        }
-    },
-    password: {
-        notEmpty: {
-            errorMessage: "La contraseña es obligatoria"
-        },
-        isPassword: {
-            errorMessage: "La contraseña es obligatoria"
-        },
-        isLength: {
-            errorMessage: "La contraseña es demasiado corta",
-            opcions: { min: 6 }
-        }
-    }
-
-})
