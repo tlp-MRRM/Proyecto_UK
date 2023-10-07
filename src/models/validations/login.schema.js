c16f8953b573b3ad11bfde0e42cfe79760fcd4ee
Meyer <meyer-hector@hotmail.com>
2023-10-07 14:58:32 -0300

    se corrigio schema login

--- src/models/validations/login.schema.js
+++ src/models/validations/login.schema.js
@@ -9,11 +9,6 @@ export const registerUserSchema = [
         .exists().withMessage('La contraseña es requerida')
         .notEmpty().withMessage('La contraseña no puede estar vacía')
         .isString().withMessage('La contraseña debe ser alfanumerica')
-        .isLength({ min: 8 }).withMessage('La contraseña debe tener minimo 8 caracteres')
-        .bail().matches(/[a-z]/).withMessage('La contraseña debe tener minimo 1 minuscula')
-        .bail().matches(/[A-Z]/).withMessage('La contraseña debe tener minimo 1 mayuscula')
-        .bail().matches(/[0-9]/).withMessage('La contraseña debe tener minimo 1 numero')
-        .bail().matches(/[^A-Za-z0-9]/).withMessage('La contraseña debe tener al menos un signo'),
 ]
 
 
