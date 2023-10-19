fc279c0a9b66b4068fddac593764f758cf0d1e7d
Meyer <meyer-hector@hotmail.com>
2023-10-19 10:14:39 -0300

    Se corrigieron espaciados en formularios

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.get('/buscar', async (req, res) => {
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port http://localhost:${port}`)
 })
 
 // END SERVER CONNECTION ---------------------------------------------------------
