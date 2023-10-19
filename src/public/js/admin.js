eb65e5d073e86335c05766f26f38afde8495f521
Meyer <meyer-hector@hotmail.com>
2023-10-19 09:23:14 -0300

    Feat: se configuro la redireccion de las paginas

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,28 +1,28 @@
 const eliminarReserva = async (e) => {
     const id = e.target.dataset.id;
-    
-        // Se pregunta al usuario si está seguro de eliminar la reserva
-        const result = await Swal.fire({
-            title: '¿Está seguro de eliminar el?',
-            text: "Esta acción no se puede deshacer",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Eliminar',
-            cancelButtonText: 'Cancelar'
-        })
-    
-        if (!result.isConfirmed) {
-            return;
-        }
-    
-    const response = await fetch(`/eliminar-usuario/${id}`,{
+
+    // Se pregunta al usuario si está seguro de eliminar la reserva
+    const result = await Swal.fire({
+        title: '¿Está seguro de eliminar el?',
+        text: "Esta acción no se puede deshacer",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Eliminar',
+        cancelButtonText: 'Cancelar'
+    })
+
+    if (!result.isConfirmed) {
+        return;
+    }
+
+    const response = await fetch(`/eliminar-usuario/${id}`, {
         method: 'DELETE',
     })
-    
+
     const data = await response.json();
-    
+
     if (response.status !== 200) {
         Swal.fire({
             title: 'Error',
@@ -31,13 +31,16 @@ const eliminarReserva = async (e) => {
             confirmButtonText: 'Aceptar'
         })
     }
-    
+
     Swal.fire({
         title: 'Usuario eliminado',
         text: data.message,
         icon: 'success',
         confirmButtonText: 'Aceptar'
     });
+    setTimeout(() => {
+        window.location.href = `http://localhost:5000/admin-users`
+    }, 2000);
 }
 const editarUsuario = (id) => {
     window.location.href = `/editar-usuario/${id}`;
