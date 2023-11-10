890eeefc13f4c5b681126294dfb9b5931fb758c9
Fabriciovich <fabriciocostadoni@gmail.com>
2023-11-10 20:04:00 -0300

    Se corrigio algunas cositas

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -7,22 +7,14 @@ export const Nav = () => {
             <header>
                 <nav>
                     <div className="mt-4 mb-4">
-                        <a href="/" > <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                            <path d="M10 12h4v4h-4z" />
-                        </svg> </a>
+                        <a href="/" className="material-symbols-outlined">
+                        home
+                        </a>
                     </div>
 
                     <div>
-                        <a href="/preguntas">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-help-circle" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-                                <path d="M12 16v.01" />
-                                <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483" />
-                            </svg>
+                        <a href="/preguntas" className="material-symbols-outlined">
+                            quiz
                         </a>
                     </div>
 
