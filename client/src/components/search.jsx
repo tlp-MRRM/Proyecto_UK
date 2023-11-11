0f6ea23e24bc03601cfdc0737e3775bcf9ed2f74
Fabriciovich <fabriciocostadoni@gmail.com>
2023-11-10 22:02:47 -0300

    Se soluciono carousel de imagenes en search, modificaciones CSS

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -17,6 +17,34 @@ export const Search = () => {
 
                         <div id="results"></div>
 
+                        <div className="mb-1 m-5">
+                            <h3 className='text-black'>Noticias destacadas</h3>
+                        </div>
+
+                        <div id="carouselExampleAutoplaying" className="carousel slide carrusel" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="../public/img/laCuencaDelPlata.jpg" className="d-block w-100" alt="La cuenca del plata" />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="../public/img/siglo21.png" className="d-block w-100" alt="Siglo 21" />
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="../public/img/UNAF.jpg" className="d-block w-100" alt="UNAF" />
+                                </div>
+                            </div>
+
+                            <button className="carousel-control-prev btn-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                                data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next btn-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                                data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
 
                     </div>
                 </section>
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { authLogin, authLogout, authRegister } from "../controllers/auth.controllers.js";
 import { getInstitutesByUser } from '../controllers/institute.controllers.js';
+import { registerSchema } from '../models/validations/authUser.js';
 
 const authRoutes = Router()
 
@@ -9,7 +10,7 @@ const authRoutes = Router()
 // Inicio de sesion de la institucion/administrador
 authRoutes.post('/api/login', authLogin);
 
-authRoutes.post('/api/register', authRegister);
+authRoutes.post('/api/register', authRegister, registerSchema);
 
 authRoutes.post('/api/get-institutes-by-user', getInstitutesByUser)
 
